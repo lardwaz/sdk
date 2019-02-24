9e2f0f8102a309fa89e1b32aa43e0aa8f0408adf
fluxy <fx@fluxy.net>
2019-02-25 01:42:20 +0400

    renamed info to summary

--- entity/meta.go
+++ entity/meta.go
@@ -2,28 +2,28 @@ package entity
 
 //Meta represents meta data for an entity in the system
 type Meta interface {
-	Info() string
+	Summary() string
 	Instance() interface{}
 	Resolvers() Resolvers
 }
 
 //NewMeta creates a a new meta object
-func NewMeta(info string, instance interface{}, resolvers Resolvers) Meta {
+func NewMeta(summary string, instance interface{}, resolvers Resolvers) Meta {
 	return meta{
-		info:      info,
+		summary:   summary,
 		instance:  instance,
 		resolvers: resolvers,
 	}
 }
 
 type meta struct {
-	info      string
+	summary   string
 	instance  interface{}
 	resolvers Resolvers
 }
 
-func (m meta) Info() string {
-	return m.info
+func (m meta) Summary() string {
+	return m.summary
 }
 
 func (m meta) Instance() interface{} {
