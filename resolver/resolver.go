dbb4acaacbb3a7bcad7987cc506cdaa2dfccd065
fluxy <fx@fluxy.net>
2019-04-29 14:41:05 +0400

    resolvers interface updated

---
+++ resolver/resolver.go
@@ -0,0 +1,77 @@
+package resolver
+
+const (
+	//Single resolvable for entity
+	Single int8 = 1
+
+	//Listing resolvable for entity
+	Listing int8 = 2
+
+	//Create resolvable for entity
+	Create int8 = 4
+
+	//Update resolvable for entity
+	Update int8 = 8
+
+	//Delete resolvable for entity
+	Delete int8 = 16
+
+	//ALL resolve to all
+	ALL int8 = Single | Listing | Create | Update | Delete
+)
+
+//Resolvable indicates if an entity can be resolved using graphql
+//and which methods to allow
+type Resolvable interface {
+	ResolverSummary() string
+	SingleResolver() bool
+	ListingResolver() bool
+	CreateResolver() bool
+	UpdateResolver() bool
+	DeleteResolver() bool
+}
+
+type resolvable struct {
+	summary string
+	single  bool
+	listing bool
+	create  bool
+	update  bool
+	delete  bool
+}
+
+func (r resolvable) ResolverSummary() string {
+	return r.summary
+}
+
+func (r resolvable) SingleResolver() bool {
+	return r.single
+}
+
+func (r resolvable) ListingResolver() bool {
+	return r.listing
+}
+
+func (r resolvable) CreateResolver() bool {
+	return r.create
+}
+
+func (r resolvable) UpdateResolver() bool {
+	return r.update
+}
+
+func (r resolvable) DeleteResolver() bool {
+	return r.delete
+}
+
+//New resolvable entity
+func New(summary string, resolvers int8) Resolvable {
+	return resolvable{
+		summary: summary,
+		single:  resolvers&Single != 0,
+		listing: resolvers&Listing != 0,
+		create:  resolvers&Create != 0,
+		update:  resolvers&Update != 0,
+		delete:  resolvers&Delete != 0,
+	}
+}
---
+++ resolver/resolver_test.go
@@ -0,0 +1,118 @@
+package resolver_test
+
+import (
+	"testing"
+
+	"go.lsl.digital/lardwaz/sdk/resolver"
+)
+
+type resolvable struct {
+	single  bool
+	listing bool
+	create  bool
+	update  bool
+	delete  bool
+}
+
+func (r resolvable) same(r2 resolver.Resolvable) bool {
+	if r.single != r2.SingleResolver() {
+		return false
+	}
+
+	if r.listing != r2.ListingResolver() {
+		return false
+	}
+
+	if r.create != r2.CreateResolver() {
+		return false
+	}
+
+	if r.update != r2.UpdateResolver() {
+		return false
+	}
+
+	if r.delete != r2.DeleteResolver() {
+		return false
+	}
+
+	return true
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name      string
+		resolvers int8
+		want      resolvable
+	}{
+		{
+			name:      "Only Single",
+			resolvers: resolver.Single,
+			want: resolvable{
+				single: true,
+			},
+		},
+		{
+			name:      "Only Listing",
+			resolvers: resolver.Listing,
+			want: resolvable{
+				listing: true,
+			},
+		},
+		{
+			name:      "Only Create",
+			resolvers: resolver.Create,
+			want: resolvable{
+				create: true,
+			},
+		},
+		{
+			name:      "Only Update",
+			resolvers: resolver.Update,
+			want: resolvable{
+				update: true,
+			},
+		},
+		{
+			name:      "Only Delete",
+			resolvers: resolver.Delete,
+			want: resolvable{
+				delete: true,
+			},
+		},
+		{
+			name:      "Single Listing",
+			resolvers: resolver.Single | resolver.Listing,
+			want: resolvable{
+				single:  true,
+				listing: true,
+			},
+		},
+		{
+			name:      "Create Update Delete",
+			resolvers: resolver.Create | resolver.Update | resolver.Delete,
+			want: resolvable{
+				create: true,
+				update: true,
+				delete: true,
+			},
+		},
+		{
+			name:      "ALL",
+			resolvers: resolver.ALL,
+			want: resolvable{
+				single:  true,
+				listing: true,
+				create:  true,
+				update:  true,
+				delete:  true,
+			},
+		},
+	}
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := resolver.New(tt.name, tt.resolvers); !tt.want.same(got) || tt.name != got.ResolverSummary() {
+				t.Errorf("%d: New() = %v, want %v", i, got, tt.want)
+			}
+		})
+	}
+}
