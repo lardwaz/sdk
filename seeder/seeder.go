7b5cbd450790a4593d8c56aab2322dfb01863ee7
Jeshta Bhoyedhur <jeshta@lsl.digital>
2019-05-20 10:33:11 +0400

    Constructor revised

--- seeder/seeder.go
+++ seeder/seeder.go
@@ -41,48 +41,26 @@ func CustomList(list string) types.CustomList {
 //--------- CreditCardI helper functions ----------//
 
 //CreditCard is a helper function that return CreditCardI
-func CreditCard(info string) types.CreditCard {
-	return types.CreditCard{
-		info: info,
-	}
+func CreditCard(info string) types.CreditCardNum {
+	return types.NewCreditCardNum(info)
 }
 
 //Date is a helper function that return DateI
 func Date(from, to int, format string) types.Date {
-	return types.Date{
-		from: from, to: to, format: format,
-	}
 }
 
 //DateTime is a helper function that return DateTimeI
 func DateTime(now bool, fromYY, toYY, fromTimeHH, toTimeHH int, format, timezone string) types.Datetime {
-	return types.Datetime{
-		now:        now,
-		fromYY:     fromYY,
-		toYY:       toYY,
-		fromTimeHH: fromTimeHH,
-		toTimeHH:   toTimeHH,
-		format:     format,
-		timezone:   timezone,
-	}
+	return types.NewDatetime(now, fromYY, toYY, fromTimeHH, toTimeHH, format, timezone)
 }
 
 //Time is a helper function that return TimeI
 func Time(from, to int, val string) types.Time {
-	return types.Time{
-		from: from,
-		to:   to,
-		val:  val,
-	}
+	return types.NewTime(from, to, val)
 
 }
 
 //Password is a helper function that return PasswordI
-func Password(min, max int, allowUpper, allowNumeric bool) types.Password {
-	return types.Password{
-		min:          min,
-		max:          max,
-		allowUpper:   allowUpper,
-		allowNumeric: allowNumeric,
-	}
+func Password(min, max int, allowUpper, allowNumeric, allowSpecialCharacters bool) types.Password {
+	return types.NewPassword(min, max, allowUpper, allowNumeric, allowSpecialCharacters)
 }
