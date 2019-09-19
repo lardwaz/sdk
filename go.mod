5c67c5b2224bd04291d05ae66747d0a78e8d063c
Jules Mikeal <julesmichaelgiovanni@gmail.com>
2019-09-19 10:55:35 +0400

    upload added

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go.lsl.digital/lardwaz/sdk
 require (
 	github.com/gorilla/mux v1.7.3
 	github.com/graphql-go/graphql v0.7.8
+	github.com/h2non/filetype v1.0.10
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 )
---
+++ upload/option.go
@@ -0,0 +1,76 @@
+package upload
+
+import (
+	"github.com/h2non/filetype/types"
+)
+
+// Options represents a set of upload options
+type Options interface {
+	Dir() string
+	SetDir(string)
+	Destination() string
+	SetDestination(string)
+	MediaPrefixURL() string
+	SetMediaPrefixURL(string)
+	FileType() []types.Type
+	AddFileType(types.Type)
+	MaxSize() int
+	SetMaxSize(sz int)
+	ConvertTo(t types.Type) types.Type
+	SetConvertTo(old types.Type, new types.Type)
+	FileTypeExist(t types.Type) bool
+}
+
+// OptionsImage represents a set of image processing options
+type OptionsImage interface {
+	MinWidth() int
+	SetMinWidth(w int)
+	MinHeight() int
+	SetMinHeight(h int)
+	Formats() OptionsFormats
+	SetFormats(opts OptionsFormats)
+}
+
+// OptionsFormats represents a list of OptionsFormats
+type OptionsFormats interface {
+	// Filter returns a OptionsFormats collection without elements filtered by fn (returning false)
+	Filter(fn func(name string, item OptionsFormat) bool) OptionsFormats
+
+	// Each loops over each item in the collection
+	Each(fn func(name string, item OptionsFormat))
+
+	// Set OptionsFormat in collection
+	Set(item OptionsFormat)
+
+	// Get a single item by name, if present
+	Get(name string) (OptionsFormat, bool)
+
+	// Length returns the number of items in the collection
+	Length() int
+}
+
+// OptionsFormat represents a set of format processing options
+type OptionsFormat interface {
+	Name() string
+	SetName(n string)
+	Width() int
+	SetWidth(w int)
+	Height() int
+	SetHeight(h int)
+	Backdrop() bool
+	SetBackdrop(b bool)
+	Watermark() OptionsWatermark
+	SetWatermark(opts ...func(OptionsWatermark))
+}
+
+// OptionsWatermark represents a set of watermark processing options
+type OptionsWatermark interface {
+	Horizontal() int
+	SetHorizontal(h int)
+	Vertical() int
+	SetVertical(v int)
+	OffsetX() int
+	SetOffsetX(x int)
+	OffsetY() int
+	SetOffsetY(y int)
+}
