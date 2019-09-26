e1134ed6c028c476c3e799a549773dad2c07aa67
fluxy <fx@fluxy.net>
2019-09-26 17:48:41 +0400

    added more information to File entity

--- types/file/file.go
+++ types/file/file.go
@@ -7,12 +7,14 @@ import (
 
 // File represents a file which may be transferred or saved.
 type File struct {
-	ID        string    `json:"id"`
-	Filename  string    `json:"filename"`
-	MIMEType  string    `json:"mimetype"`
-	Filepath  string    `json:"filepath"`
-	Size      int       `json:"size"`
-	CreatedAt time.Time `json:"created_at"`
+	ID          string    `json:"id"`
+	ContentID   string    `json:"content_id"`
+	ContentType string    `json:"content_type"`
+	Filename    string    `json:"filename"`
+	MIMEType    string    `json:"mimetype"`
+	Filepath    string    `json:"filepath"`
+	Size        int       `json:"size"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 // New creates a new instance of File from a json string
