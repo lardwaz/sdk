6c084f7e44c7ee16aa3b0826462e69d307cf8786
Jeshta Bhoyedhur <jeshta@lsl.digital>
2019-10-11 14:18:36 +0400

    updates  in sdk seeds

--- seeder/types/customlist.go
+++ seeder/types/customlist.go
@@ -1,18 +1,23 @@
 package types
 
+import (
+	"strings"
+)
+
 //CustomList has a list where each list seperated by comma ,
 type CustomList struct {
-	options string
+	options []string
 }
 
 //NewCustomList is a constructor for CustomList
 func NewCustomList(options string) CustomList {
+	opt := strings.Split(options, ",")
 	return CustomList{
-		options: options,
+		options: opt,
 	}
 }
 
 //Options is a helper function that implements Options() from CustomList interface
-func (c CustomList) Options() string {
+func (c CustomList) Options() []string {
 	return c.options
 }
